f3f05ac81186ca838486011a3f3b7c917faa00dd
iamaashi23 <101196053+iamaashi23@users.noreply.github.com>
2023-04-16 21:29:25 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages';
+import LoginPage from './components/pages/login';
+import ContactPage from './components/pages/contact';
+import PageNotFoundPage from './components/pages/pagenotfound';
+import ReqApi from './components/Login/API/ReqApi';
+import PostApi from './components/Login/API/PostApi'
+import Notepad from './components/Notepad'
+import Bmi from './components/pages/bmi';
+import Calorie from './components/pages/calorie'
+import Categories from './components/pages/categories';
+import Fit from './components/pages/excersisesformen';
+import Ref from './components/pages/toloosefat';
+import Fitness from './components/pages/workout';
+function App() {
+  return (
+    <Router>
+
+      <Routes>
+        <Route path="/" element={<Home />} />  
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/contact" element={<ContactPage/>} />
+        <Route path="/*" element={<PageNotFoundPage/>} />
+         <Route path='/register' element={<ReqApi />}/>
+          <Route path= '/post'  element={<PostApi />}/>
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/excercisesformen" element={<Fit />} />
+          <Route path="/toloosefat" element={<Ref />} />
+          <Route path="/workout" element={<Fitness />} /> 
+          <Route path="/notepad" element={<Notepad />} />  
+          <Route path="/bmi" element={<Bmi/>} />
+          <Route path="/calorie" element={<Calorie/>} />
+        
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
